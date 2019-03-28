4971e1a9cd7152f532803effae29cad899c4c673
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-03-28 07:21:08 -0300

    Desenvolvimento exercicios curso parte2

--- README.md
+++ README.md
@@ -11,5 +11,14 @@ Projeto feito durante a realização dos curso da carreira desenvolvedor Python
 * Laços for e while;
 * Estruturas de decisão if, elif e else
 * Modularização : diferença entre chamadas executando arquivos python como principal e usando como import
-    > variável name preenchido com main ao ser executado como programa principal:
-    __name__=="__main__"
+    > variável name preenchido com main ao ser executado como programa principal
+ 
+ ### Itens estudados em Python 3 parte 2: Avançando na linguagem:
+ 
+ * Operadores: not, and
+ * String métodos: https://docs.python.org/3/library/stdtypes.html#text-sequence-type-str
+ * tuple: sequência imutável > usa parênteses na declaração
+ * list: sequência mutável > usa colchetes na declaração
+ * set: coleção não ordenada (sem repetição) > usa chaves na declaração
+ * dictionary: par chave, valor > {key:value, key2:value2}
+ * List Comprehension . Exemplo: letras_acertadas = ["_" for letra in palavra_secreta]
--- forca.py
+++ forca.py
@@ -3,13 +3,38 @@ def jogar():
     print("********** ******** Jogo da Forca! ********** ******* ")
     print("******************************************************")
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+
+    palavra_secreta = "banana".upper()
     enforcou = False
     acertou = False
 
-    while(not enforcou and not acertou):
-        print("Jogando")
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    erros = 0
 
+    print(letras_acertadas)
+
+    while(not enforcou and not acertou):
+        chute = input("Qual a letra?")
+        chute = chute.strip().upper()
+        index = 0
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[index] = letra
+                index = index + 1
+            letras_faltando = int(letras_acertadas.count('_'))
+            if (letras_faltando == 0):
+                acertou = True
+        else:
+            erros = erros + 1
+        enforcou = erros == 6
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou")
+    else:
+        print("Você perdeu")
     print("Fim do Jogo")
 
 if(__name__=="__main__"):
