aa3628b4bff41de605da9fdea0e7d78e94abcf45
izabelrodrigues <izabel.castro.rodrigues@gmail.com>
2019-03-28 21:50:14 -0300

    reestruturando o jogo forca: extraindo funções

--- forca.py
+++ forca.py
@@ -1,9 +1,41 @@
 import random
+
+
 def jogar():
+    imprime_mensagem_abertura()
+    palavra = carrega_palavra_secreta()
+    letras_acertadas = inicializa_letras_acertadas(palavra)
+    print(letras_acertadas)
+
+    enforcou = False
+    acertou = False
+    erros = 0
+
+    while (not enforcou and not acertou):
+        chute = obtem_chute_jogador()
+        index = 0
+        if (chute in palavra):
+            marca_chute_correto(palavra, chute, letras_acertadas, index)
+            acertou = verifica_se_acertou(letras_acertadas, acertou)
+        else:
+            erros = erros + 1
+        enforcou = erros == 6
+        print(letras_acertadas)
+
+    if (acertou):
+        imprime_mensagem_vencedor()
+    else:
+        imprime_mensagem_perdedor()
+    print("Fim do Jogo")
+
+
+def imprime_mensagem_abertura():
     print("******************************************************")
     print("********** ******** Jogo da Forca! ********** ******* ")
     print("******************************************************")
 
+
+def carrega_palavra_secreta():
     arquivo = open("palavras.txt", "r")
     palavras = []
 
@@ -15,38 +47,39 @@ def jogar():
 
     numero = random.randrange(0, len(palavras))
     palavra_secreta = palavras[numero].upper()
+    return palavra_secreta
 
-    enforcou = False
-    acertou = False
 
-    letras_acertadas = ["_" for letra in palavra_secreta]
-    erros = 0
+def marca_chute_correto(palavra, chute, letras_acertadas, index):
+    for letra in palavra:
+        if (chute == letra):
+            letras_acertadas[index] = letra
+        index = index + 1
 
-    print(letras_acertadas)
 
-    while(not enforcou and not acertou):
-        chute = input("Qual a letra?")
-        chute = chute.strip().upper()
-        index = 0
-        if(chute in palavra_secreta):
-            for letra in palavra_secreta:
-                if(chute == letra):
-                    letras_acertadas[index] = letra
-                index = index + 1
-            letras_faltando = int(letras_acertadas.count('_'))
-            if (letras_faltando == 0):
-                acertou = True
-        else:
-            erros = erros + 1
-        enforcou = erros == 6
-        print(letras_acertadas)
+def inicializa_letras_acertadas(palavra_secreta):
+    return ["_" for letra in palavra_secreta]
 
-    if(acertou):
-        print("Você ganhou")
-    else:
-        print("Você perdeu")
-    print("Fim do Jogo")
 
-if(__name__=="__main__"):
+def obtem_chute_jogador():
+    chute = input("Qual a letra?")
+    chute = chute.strip().upper()
+    return chute
+
+
+def verifica_se_acertou(letras_acertadas, acertou):
+    letras_faltando = int(letras_acertadas.count('_'))
+    if (letras_faltando == 0):
+        acertou = True
+    return acertou
+
+def imprime_mensagem_vencedor():
+    print("Você ganhou!")
+
+
+def imprime_mensagem_perdedor():
+    print("Você perdeu!")
+
+
+if (__name__ == "__main__"):
     jogar()
-
